d93a6de72acdd4dca9e3a08883af38551b63ecd3
joldenstam <joldenstam@qualogy.com>
2020-01-13 22:02:24 -0300

    Created builder design pattern

--- src/main/java/sr/unasat/jpa/designPatterns/builder/Builder.java
+++ src/main/java/sr/unasat/jpa/designPatterns/builder/Builder.java
@@ -1,11 +1,21 @@
 package sr.unasat.jpa.designPatterns.builder;
 
+import sr.unasat.jpa.entity.Computer;
 import sr.unasat.jpa.entity.ComputerConfig;
 import sr.unasat.jpa.entity.Hardware;
 
-public class Builder extends ComputerConfig {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Builder {
+
+    private Double price = 0.00D;
     private Hardware memory, gpu, storage, cpu;
+    private boolean bluetooth, thunderbolt, lte, touchScreen, twoInOne;
+
+    public Double getPrice() {
+        return this.price;
+    }
 
     public Builder setMemory(Hardware memory) {
         this.memory = memory;
@@ -29,50 +39,61 @@ public class Builder extends ComputerConfig {
 
     public Builder withBluetooth() {
         this.bluetooth = true;
+        this.price += 30.00D;
         return this;
     }
 
     public Builder withThunderbolt() {
         this.thunderbolt = true;
+        this.price += 45.00D;
         return this;
     }
 
     public Builder withLte() {
         this.lte = true;
+        this.price += 65.00D;
         return this;
     }
 
     public Builder withTouchScreen() {
         this.touchScreen = true;
+        this.price += 110.00D;
         return this;
     }
 
     public Builder withTwoInOne() {
         this.twoInOne = true;
+        this.price += 60.00D;
         return this;
-    }
-
-    public boolean getBluetooth() {
-        return this.bluetooth;
-    }
-
-    public boolean getTouchScreen() {
-        return this.touchScreen;
-    }
-
-    public boolean getTwoInOne() {
-        return this.twoInOne;
-    }
-
-    public boolean getLte() {
-        return this.lte;
-    }
-
-    public boolean getThunderbolt() {
-        return this.thunderbolt;
     }
 
     public ComputerConfig build() {
-        return this;
+        return getConfig();
+    }
+
+    public ComputerConfig build(Computer computer) {
+            ComputerConfig config = getConfig();
+            config.setComputer(computer);
+            return config;
+        }
+
+    private ComputerConfig getConfig() {
+        final ComputerConfig config = new ComputerConfig();
+        List<Hardware> hardwareList = new ArrayList<Hardware>() {{
+            add(memory);
+            add(gpu);
+            add(cpu);
+            add(storage);
+        }};
+
+        config.setHardwareList(hardwareList);
+        config.setBluetooth(bluetooth);
+        config.setThunderbolt(thunderbolt);
+        config.setLte(lte);
+        config.setTouchScreen(touchScreen);
+        config.setTwoInOne(twoInOne);
+        config.setSubTotal(this.price);
+
+        return config;
     }
 }
