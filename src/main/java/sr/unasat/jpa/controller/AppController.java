57338d4472f78b75c3b5f1fa8ec4d8e28b4c1b01
joldenstam <joldenstam@qualogy.com>
2020-01-14 23:33:20 -0300

    Added application controller

---
+++ src/main/java/sr/unasat/jpa/controller/AppController.java
@@ -0,0 +1,82 @@
+package sr.unasat.jpa.controller;
+
+import sr.unasat.jpa.config.JPAConfiguration;
+import sr.unasat.jpa.service.impl.*;
+
+import javax.persistence.EntityManager;
+
+public class AppController {
+
+    private BrandService brandService;
+    private CategoryService categoryService;
+    private ComputerConfigService computerConfigService;
+    private ComputerService computerService;
+    private CustomerService customerService;
+    private HardwareService hardwareService;
+    private HardwareStockService hardwareStockService;
+    private PaymentTypeService paymentTypeService;
+    private ReceiptService receiptService;
+    private RoleService roleService;
+    private UserService userService;
+
+    public AppController() {
+        final EntityManager entityManager = JPAConfiguration.getEntityManager();
+
+        //Initialize services
+        brandService = new BrandService(entityManager);
+        categoryService = new CategoryService(entityManager);
+        computerConfigService = new ComputerConfigService(entityManager);
+        computerService = new ComputerService(entityManager);
+        customerService = new CustomerService(entityManager);
+        hardwareService = new HardwareService(entityManager);
+        hardwareStockService = new HardwareStockService(entityManager);
+        paymentTypeService = new PaymentTypeService(entityManager);
+        receiptService = new ReceiptService(entityManager);
+        roleService = new RoleService(entityManager);
+        userService = new UserService(entityManager);
+    }
+
+    public BrandService getBrandService() {
+        return brandService;
+    }
+
+    public CategoryService getCategoryService() {
+        return categoryService;
+    }
+
+    public ComputerConfigService getComputerConfigService() {
+        return computerConfigService;
+    }
+
+    public ComputerService getComputerService() {
+        return computerService;
+    }
+
+    public CustomerService getCustomerService() {
+        return customerService;
+    }
+
+    public HardwareService getHardwareService() {
+        return hardwareService;
+    }
+
+    public HardwareStockService getHardwareStockService() {
+        return hardwareStockService;
+    }
+
+    public PaymentTypeService getPaymentTypeService() {
+        return paymentTypeService;
+    }
+
+    public ReceiptService getReceiptService() {
+        return receiptService;
+    }
+
+    public RoleService getRoleService() {
+        return roleService;
+    }
+
+    public UserService getUserService() {
+        return userService;
+    }
+}
